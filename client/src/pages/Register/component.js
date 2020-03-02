d9793b9fa14f9ec85224638f57a85b439002c78b
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-03-02 11:45:26 +0700

    Register Route Setup

---
+++ client/src/pages/Register/component.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class component extends Component {
+    render() {
+        return <h1>Register Page</h1>;
+    }
+}
+
+export default component;
