d1d9107f553850bb7b715d9c7c5187023590164e
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-03-02 12:04:18 +0700

    Add register component

---
+++ client/src/components/forms/register/component.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export class Component extends React.Component {
+    render() {
+        return (
+            <form>
+                <section>
+                    <label>Username</label>
+                    <input type="text" placeholder="Your Username" required />
+                </section>
+                <section>
+                    <label>Full Name</label>
+                    <input type="text" placeholder="Your Full Name" required />
+                </section>
+                <section>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        placeholder="example@example.com"
+                        required
+                    />
+                </section>
+                <section>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        placeholder="Make it Secure!"
+                        required
+                    />
+                </section>
+                <button type="submit">Register Me</button>
+            </form>
+        );
+    }
+}
+
+export default Component;
--- client/src/pages/Register/component.js
+++ client/src/pages/Register/component.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import RegisterForm from "../../components/forms/register";
 
 export class component extends Component {
     render() {
-        return <h1>Register Page</h1>;
+        return (
+            <main>
+                <h1>Register</h1>
+                <RegisterForm />
+            </main>
+        );
     }
 }
 
