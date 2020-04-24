b2897bc0e287004de61210a5471ddfd3d1d86d87
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-04-24 15:46:28 +0700

    Add - Search title

--- client/src/pages/Search/component.js
+++ client/src/pages/Search/component.js
@@ -2,8 +2,16 @@ import React, { Component } from "react";
 import PageBase from "../../components/layouts/PageBase";
 
 export class component extends Component {
+  componentDidMount() {
+    document.title = "BaKar | No result";
+  }
+
   render() {
-    return <PageBase>Search</PageBase>;
+    return (
+      <PageBase>
+        <h1>Found 0 Result</h1>
+      </PageBase>
+    );
   }
 }
 
